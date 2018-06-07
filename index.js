dccf64bf78f2148b230dfa994f65674b23ebd0bb
HUGODOS <laurence.jones@live.co.uk>
2018-06-07 12:58:18 +0100

    Update index.js

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const {app, BrowserWindow} = require('electron')
     }))
   
     // Open the DevTools.
-    win.webContents.openDevTools()
+    //win.webContents.openDevTools()
   
     // Emitted when the window is closed.
     win.on('closed', () => {
